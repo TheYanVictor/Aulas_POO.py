488a3196331fbd7802f8bdada6f3c092abd3afbb
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-09-05 13:27:09 +0000

    Att

---
+++ src/aula_05_09_2023/exemplo_03_heranca.py
@@ -0,0 +1,17 @@
+#Conceito de herança
+#-------------------
+
+class Pessoas:
+    def __init__(self):
+        self.__nome = "Alberto Niriz"
+        self.__cpf = "4544546551684351"
+        self.__celular = "94965464365"
+        
+class Alunos:
+    def __init__(self):
+        self.ra = "545454545"
+
+class Professores:  
+    def __init__(self):
+        self.func = "222222"
+
