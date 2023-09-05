7e35059719a2d7a5e2f0f86ad0b3b86ac95bdd6c
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-09-05 13:44:32 +0000

    Final

--- src/aula_05_09_2023/exemplo_03_heranca.py
+++ src/aula_05_09_2023/exemplo_03_heranca.py
@@ -3,15 +3,21 @@
 
 class Pessoas:
     def __init__(self):
-        self.__nome = "Alberto Niriz"
-        self.__cpf = "4544546551684351"
-        self.__celular = "94965464365"
-        
+        self.nome = "Alberto Niriz"
+        self.cpf = "4544546551684351"
+        self.celular = "94965464365"
 class Alunos:
     def __init__(self):
         self.ra = "545454545"
+        Pessoas.__init__(self)
 
 class Professores:  
     def __init__(self):
         self.func = "222222"
+        Pessoas.__init__(self)
 
+a1 = Alunos()
+print(a1.ra)
+
+p1 = Professores()
+print(p1.nome)
