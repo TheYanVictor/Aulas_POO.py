956975722a4f2990915b13f2bf692e1b4552b109
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-09-19 11:29:29 +0000

    att

--- src/aula_19_09_2023/exercicio01.py
+++ src/aula_19_09_2023/exercicio01.py
@@ -1,1 +1,17 @@
 #Polimorfismo
+
+class Pagamentos:
+    def pagar(self):
+        print("Pagamento realizado!")
+
+class Dinheiro(Pagamentos):
+    def pagar(self):
+        print("Pagamentos em dinheiro!")
+
+class Cartao(Pagamentos):
+    def pagar(self):
+        print("Pagamentos em cartao!")
+
+
+obj1 = Pagamentos
+obj1.pagar()
