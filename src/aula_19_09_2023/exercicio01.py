ca33387ba938c5ed8ff8cc1af9b9625f3cc2f147
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-09-19 11:30:27 +0000

    att

--- src/aula_19_09_2023/exercicio01.py
+++ src/aula_19_09_2023/exercicio01.py
@@ -11,7 +11,10 @@ class Dinheiro(Pagamentos):
 class Cartao(Pagamentos):
     def pagar(self):
         print("Pagamentos em cartao!")
+class Pix(Pagamentos):
+    def pagar(self):
+        print("Pagamento com Pix!")
 
-
-obj1 = Pagamentos
+obj1 = Pix()
 obj1.pagar()
+
