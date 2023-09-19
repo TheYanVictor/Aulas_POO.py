2d785bb8fb45c38a2ab34f677b8f593d6c17b69f
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-09-19 11:33:32 +0000

    att

--- src/aula_19_09_2023/exercicio01.py
+++ src/aula_19_09_2023/exercicio01.py
@@ -4,17 +4,27 @@ class Pagamentos:
     def pagar(self):
         print("Pagamento realizado!")
 
+#Classe dinheiro herdando os métodos da classe pagamentos
 class Dinheiro(Pagamentos):
     def pagar(self):
         print("Pagamentos em dinheiro!")
 
+#Classe cartão herdando os métodos da classe pagamentos
 class Cartao(Pagamentos):
     def pagar(self):
         print("Pagamentos em cartao!")
+
+#Classe pix herdando os métodos da classe pagamentos
 class Pix(Pagamentos):
     def pagar(self):
         print("Pagamento com Pix!")
 
-obj1 = Pix()
-obj1.pagar()
+#Instanciando os objetos
+obj1 = Dinheiro()
+obj2 = Cartao()
+obj3 = Pix()
 
+#Chamando as funções
+obj1.pagar()
+obj2.pagar()
+obj3.pagar()
