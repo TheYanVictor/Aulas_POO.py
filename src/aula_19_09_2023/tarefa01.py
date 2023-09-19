37efe25feccf0ce3d4631a680bc8e8a68ca894e9
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-09-19 13:59:57 +0000

    Att

--- src/aula_19_09_2023/tarefa01.py
+++ src/aula_19_09_2023/tarefa01.py
@@ -1,6 +1,31 @@
 #Tarefa de polimorfismo - WIP
 
 #Classe primaria
+class Locacao:
+    def __init__(self):
+        self.prazo = 0
+        
+    def locar(self, prazo):
+        self.prazo = prazo
+        
+    def exibir(self):
+        return(self.prazo)
+    
+#Classes polimorficas
+class Localuno(Locacao):
+    def locar(self):
+        self.prazo = 10
+        
+class Locprof(Locacao):
+    def locar(self):
+        self.prazo = 20
+        
+aluno = Localuno()
+aluno.locar()
+print("Aluno - " , aluno.exibir())
 
+professor = Locprof()
+professor.locar()
+print("Professor - ", professor.exibir())
 
 
