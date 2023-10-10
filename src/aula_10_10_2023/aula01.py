e94381cf3893cb7ac2035222edeb5bc195673c93
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-10-10 13:16:26 +0000

    Add aula01

---
+++ src/aula_10_10_2023/aula01.py
@@ -0,0 +1,22 @@
+# Classe abstrata e Interface
+from abc import ABC, abstractmethod
+
+class Media(ABC):
+    notas = []
+    
+    @abstractmethod
+    def calcula(self):
+        pass
+    
+class MediaBemestre(Media):
+    def __init__(self, notas):
+        self.notas = notas 
+    def calcula(self):
+        calc = (p1 + p2)/2
+        return (calc)
+    
+
+p1 = 7
+p2 = 2
+obj1 = MediaBemestre([p1,p2])
+print(obj1.calcula())
